4873facbf28493853576eb40c9d2f6c93e8dbedb
Conor O'Brien <obrienceob@gmail.com>
2021-03-07 15:29:49 -0800

    adding in routes for api and html'

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = (app) => {
+    // get and display the notes page
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            res.json(JSON.parse(data));
+        });
+    });
+
+    // posting new notes to the notes page
+    app.post('api/notes', (req, res) => {
+        let notesTaken = req.body;
+        notesTaken.id = uuidv4();
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            let notes = JSON.parse(data);
+            notes.push(notesTaken);
+            fs.writeFile('./db/db.json'. JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json(notes);
+            });
+        });
+    });
+
+    //deleting notes page
+    app.delete('api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            const notes = JSON.parse(data);
+            let notesArr = notes.filter((note) => {
+                return id !== note.id;
+            })
+            fs.writeFile('./db/db.json', JSON.stringify(notesArr), (err) => {
+                if (err) throw err;
+                res.json(notesArr);
+            });
+        });
+    });
+}
