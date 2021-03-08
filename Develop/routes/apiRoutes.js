2f4e37ccd89860b20959f77614dac5604603ad7e
Conor O'Brien <obrienceob@gmail.com>
2021-03-07 17:15:37 -0800

    uddated routes files

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,9 +1,45 @@
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+// module.exports = (app) => {
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//         if (err) throw err;
+//         var notes = JSON.parse(data);
+
+//         app.get('api/notes', (req, res) => {
+//             res.json(notes);
+//         });
+
+//         app.post('api/notes', (req, res) => {
+//             let newNote = req.body;
+//             newNote.id = uuidv4();
+//             notes.push(newNote);
+//             updateNotes();
+//             return console.log("added new note: " + newNote.title);
+//         });
+
+//         app.get('api/notes/:id', (req, res) => {
+//             res.json(notes[req.params.id]);
+//         });
+
+//         app.delete('api/notes/:id', (req, res) => {
+//             notes.splice(req.params.id, 1);
+//             updateNotes();
+//             console.log('deleted note with id' + req.params.id);
+//         });
+
+//         function updateNotes() {
+//             fs.writeFile("./db/db.json",JSON.stringify(notes,'\t'),err => {
+//                 if (err) throw err;
+//                 return true;
+//             });
+//         }
+//     });
+// }
+
 module.exports = (app) => {
     // get and display the notes page
-    app.get('/api/notes', (req, res) => {
+    app.get("/api/notes", (req, res) => {
         fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
             res.json(JSON.parse(data));
@@ -11,14 +47,14 @@ module.exports = (app) => {
     });
 
     // posting new notes to the notes page
-    app.post('api/notes', (req, res) => {
+    app.post("/api/notes", (req, res) => {
         let notesTaken = req.body;
         notesTaken.id = uuidv4();
         fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
             let notes = JSON.parse(data);
             notes.push(notesTaken);
-            fs.writeFile('./db/db.json'. JSON.stringify(notes), (err) => {
+            fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
                 if (err) throw err;
                 res.json(notes);
             });
@@ -26,7 +62,7 @@ module.exports = (app) => {
     });
 
     //deleting notes page
-    app.delete('api/notes/:id', (req, res) => {
+    app.delete("/api/notes/:id", (req, res) => {
         const id = req.params.id;
         fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 //defiining the paths for the notes page and the index landing page
 module.exports = (app) => {
-    app.get('/notes', (req, res) => {
+    app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
