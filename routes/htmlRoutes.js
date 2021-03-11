c985ac45dc42eb7d3c685abc8fe97c0255154b5c
Conor O'Brien <obrienceob@gmail.com>
2021-03-10 22:09:30 -0800

    trying again server.js on root as well

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,10 +4,10 @@ const path = require('path');
 //defiining the paths for the notes page and the index landing page
 module.exports = (app) => {
     app.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+        res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
     });
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(__dirname, '../Develop/public/index.html'));
     });
 }
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //sets express app to serve static files
-app.use(express.static('public'));
+app.use(express.static('./Develop/public'));
 
 //requiring the routes folder where api and html routes are stored
 require('./routes/apiRoutes')(app);
